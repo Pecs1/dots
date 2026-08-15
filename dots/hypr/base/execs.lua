4af1883be59062cd56d553beadf9c2deb6e6325d
Pecs1 <173087731+Pecs1@users.noreply.github.com>
2026-08-15 11:28:09 +0200

    add hyprpolkit

--- dots/hypr/base/execs.lua
+++ dots/hypr/base/execs.lua
@@ -4,6 +4,7 @@ hl.on("hyprland.start", function ()
      hl.exec_cmd("waybar")
      hl.exec_cmd("hypridle")
      hl.exec_cmd("gnome-keyring-daemon --start --components=secrets")
+     hl.exec_cmd("systemctl --user start hyprpolkitagent")
 
      hl.exec_cmd("wl-paste --type text --watch cliphist store")
      hl.exec_cmd("wl-paste --type image --watch cliphist store")
